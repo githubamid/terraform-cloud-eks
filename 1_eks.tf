ec37917e0b92024d0ec2b17670b3b6bba77f61d0
Dzmitry Khomich <dzmitry.khomich@prezzee.com>
2023-11-14 12:48:56 +0300

    add variables

--- 1_eks.tf
+++ 1_eks.tf
@@ -27,12 +27,7 @@ resource "aws_eks_cluster" "demo" {
   role_arn = aws_iam_role.demo.arn
 
   vpc_config {
-    subnet_ids = [
-      aws_subnet.private-us-east-1a.id,
-      aws_subnet.private-us-east-1b.id,
-      aws_subnet.public-us-east-1a.id,
-      aws_subnet.public-us-east-1b.id
-    ]
+    subnet_ids = var.subnet_ids
   }
 
   depends_on = [aws_iam_role_policy_attachment.demo-AmazonEKSClusterPolicy]
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "subnet_ids" {
+    type = list
+    default = ["subnet-07422a15f0b0782f2","subnet-08be2ad125c37d10c","subnet-0fdc06a336f952cc0"]
+}
